e5025f9069e6c5a36c39efdf8c92b5a8b8bba26c
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2024-02-03 21:36:24 +0530

    add posts section to the dashboard

--- client/src/components/DashSidebar.tsx
+++ client/src/components/DashSidebar.tsx
@@ -1,14 +1,16 @@
 import { Sidebar } from "flowbite-react";
 import { useEffect, useState } from "react";
-import { HiArrowSmRight, HiUser } from "react-icons/hi";
-import { useDispatch } from "react-redux";
+import { HiArrowSmRight, HiDocumentText, HiUser } from "react-icons/hi";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
 import { signOutSuccess } from "../redux/user/userSlice";
+import { RootState } from "../redux/store";
 
 export default function DashSidebar() {
   const dispatch = useDispatch();
   const location = useLocation();
   const [tab, setTab] = useState("");
+  const { currentUser } = useSelector((state: RootState) => state.user);
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -35,18 +37,29 @@ export default function DashSidebar() {
   return (
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
-        <Sidebar.ItemGroup>
+        <Sidebar.ItemGroup className="flex flex-col gap-1">
           <Link to="/dashboard?tab=profile">
             <Sidebar.Item
               active={tab === "profile"}
               icon={HiUser}
-              label={"User"}
+              label={currentUser?.isAdmin ? "Admin" : "User"}
               labelColor="dark"
               as="div"
             >
               Profile
             </Sidebar.Item>
           </Link>
+          {currentUser?.isAdmin && (
+            <Link to="/dashboard?tab=posts">
+              <Sidebar.Item
+                active={tab === "posts"}
+                icon={HiDocumentText}
+                as="div"
+              >
+                Posts
+              </Sidebar.Item>
+            </Link>
+          )}
 
           <Sidebar.Item
             icon={HiArrowSmRight}
