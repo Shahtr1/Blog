c995abb7eb52b75418c6fa7a2c50edc152d21f9b
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2024-02-05 15:07:15 +0530

    create get posts api route

--- api/controllers/post.controller.ts
+++ api/controllers/post.controller.ts
@@ -35,3 +35,62 @@ export const createPost = async (
     next(error);
   }
 };
+
+export const getPosts = async (
+  req: ERequest,
+  res: EResponse,
+  next: NextFunction
+) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex as string) || 0;
+    const limit = parseInt(req.query.limit as string) || 9;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const query: Record<any, any> = {};
+
+    if (req.query.userId) {
+      query.userId = req.query.userId;
+    }
+    if (req.query.category) {
+      query.category = req.query.category;
+    }
+    if (req.query.slug) {
+      query.slug = req.query.slug;
+    }
+    if (req.query.postId) {
+      query._id = req.query.postId;
+    }
+    if (req.query.searchTerm) {
+      query.$or = [
+        { title: { $regex: req.query.searchTerm, $options: "i" } },
+        { content: { $regex: req.query.searchTerm, $options: "i" } },
+      ];
+    }
+
+    const posts = await Post.find(query)
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const totalPosts = await Post.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthsPosts = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      posts,
+      totalPosts,
+      lastMonthsPosts,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/post.route.ts
+++ api/routes/post.route.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser";
-import { createPost } from "../controllers/post.controller";
+import { createPost, getPosts } from "../controllers/post.controller";
 
 const postRoutes = express.Router();
 
 postRoutes.post("/create", verifyToken, createPost);
+postRoutes.get("/getposts", getPosts);
 
 export default postRoutes;
