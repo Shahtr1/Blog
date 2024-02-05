71f19554d6941d1f1fa3987d7931b30ff0814e29
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2024-02-05 18:00:01 +0530

    add show more functionality to the posts result of the dashboard

--- client/src/components/DashPosts.tsx
+++ client/src/components/DashPosts.tsx
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 export default function DashPosts() {
   const { currentUser } = useSelector((state: RootState) => state.user);
   const [userPosts, setUserPosts] = useState<IPost[]>([]);
+  const [showMore, setShowMore] = useState(true);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -18,6 +19,9 @@ export default function DashPosts() {
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
+          if (data.posts?.length < 9) {
+            setShowMore(false);
+          }
         }
       } catch (error: any) {
         console.error(error.message);
@@ -27,6 +31,25 @@ export default function DashPosts() {
       fetchPosts();
     }
   }, [currentUser!._id]);
+
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(
+        `/api/post/getposts?userId=${currentUser!._id}&startIndex=${startIndex}`
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setUserPosts([...userPosts, ...data.posts]);
+        if (data.posts.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error: any) {
+      console.error(error.message);
+    }
+  };
+
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
       {currentUser?.isAdmin && userPosts.length > 0 ? (
@@ -83,6 +106,14 @@ export default function DashPosts() {
               </Table.Body>
             ))}
           </Table>
+          {showMore && (
+            <button
+              onClick={handleShowMore}
+              className="w-full text-teal-500 self-center text-sm py-7"
+            >
+              Show more
+            </button>
+          )}
         </>
       ) : (
         <p>You have no posts yet</p>
