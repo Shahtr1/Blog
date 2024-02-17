77cd0c203d776c8d095c3c13b75d1b2b0c62050d
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2024-02-17 22:48:47 +0530

    complete about and projects pages

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,6 +1,42 @@
+import { Link } from "react-router-dom";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="min-h-screen flex items-center justify-center">
+      <div className=" max-w-2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About Clay's Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Welcome to Clay's Blog! This blog was created by{" "}
+              <Link
+                to={"https://www.clayster.net"}
+                className="text-teal-400 hover:underline"
+              >
+                Shahrukh Tramboo
+              </Link>{" "}
+              as a personal project to share his thoughts and ideas with the
+              world. Shahrukh is a passionate developer who loves to write about
+              technology, coding, and everything in between.
+            </p>
+            <p>
+              On this blog, you'll find weekly articles and tutorials on topics
+              such as web development, software engineering, programming
+              languages, cybersecurity, operating systems, and much more.
+              Shahrukh is always learning and exploring new technologies, so be
+              sure to check back often for new content!
+            </p>
+            <p>
+              We encourage you to leave comments on our posts and engage with
+              other readers. You can like other people's comments and reply to
+              them as well. We believe that a community of learners can help
+              each other grow and improve
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
